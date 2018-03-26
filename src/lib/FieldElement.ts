dea709d91f072b057a9c10d17a27a9140dd14820
Rich DeYoung <12941684+rbdeyoung@users.noreply.github.com>
2018-03-25 23:40:14 -0500

    Field Element addition

--- src/lib/FieldElement.ts
+++ src/lib/FieldElement.ts
@@ -2,12 +2,24 @@ export class FieldElement {
     public num: number
     public prime: number
 
-    constructor(num: number, prime: number) {
+    public constructor(num: number, prime: number) {
         if (num >= prime || num < 0) {
             throw new RangeError(`Num ${num} not in prime field range 0 to ${prime-1}`)
             // throw new Error()
         }
         this.num = num
         this.prime = prime
+    }
+
+    public equals(fieldElement: FieldElement): boolean {
+        return fieldElement.prime === this.prime &&
+            fieldElement.num === this.num
+    }
+
+    public add(fieldElement: FieldElement): FieldElement {
+        if(fieldElement.prime !== this.prime) {
+            throw new Error('Elements are not of the same prime field')
+        }
+        return new FieldElement((this.num + fieldElement.num) % this.prime, this.prime)
     }
 }
--- src/lib/tests/FieldElement.unit.ts
+++ src/lib/tests/FieldElement.unit.ts
@@ -22,4 +22,44 @@ describe('FieldElement', () => {
             })
         })
     })
+
+    describe('equality', () => {
+        const fe = new FieldElement(5,7)
+        describe('success', () => {
+
+            it('should return true if item matches', () => {
+                const fe2 = new FieldElement(5, 7)
+                expect(fe.equals(fe2)).toBe(true)
+            })
+        })
+
+        describe('failure', () => {
+            it('should return false if prime does not match', () => {
+                const fe2 = new FieldElement(5,11)
+                expect(fe.equals(fe2)).toBe(false)
+            })
+
+            it('should return false if num does not match', () => {
+                const fe2 = new FieldElement(3, 7)
+                expect(fe.equals(fe2)).toBe(false)
+            })
+        })
+    })
+
+    describe('addition', () => {
+        const fe = new FieldElement(5,7)
+
+        describe('success', () => {
+            it('should add two elements', () => {
+                const fe2 = new FieldElement(3, 7)
+                expect(fe.add(fe2)).toEqual(new FieldElement(1,7))
+            })
+        })
+        describe('failure', () => {
+            it('should fail to add elements not of the same prime field', () => {
+                const fe2 = new FieldElement(3,11)
+                expect(() => fe.add(fe2)).toThrow(Error)
+            })
+        })
+    })
 })
