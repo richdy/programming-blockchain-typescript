568061455228b601017f76322e50b81f258603d9
Rich DeYoung <12941684+rbdeyoung@users.noreply.github.com>
2018-03-31 18:35:26 -0500

    Division, Negative Exponentiation

--- src/lib/FieldElementBN.ts
+++ src/lib/FieldElementBN.ts
@@ -3,6 +3,7 @@ import { BigInteger } from 'jsbn'
 export class FieldElementBN {
     public num: BigInteger
     public prime: BigInteger
+    private readonly TWO: BigInteger = new BigInteger('2')
 
     public constructor(num: BigInteger, prime: BigInteger) {
         if (num.compareTo(prime) >= 0 || num.compareTo(BigInteger.ZERO) < 0) {
@@ -38,7 +39,24 @@ export class FieldElementBN {
         return new FieldElementBN((this.num.multiply(fieldElement.num)).mod(this.prime), this.prime)
     }
 
-    public pow(power: number): FieldElementBN {
+    public pow(power: BigInteger): FieldElementBN {
+        if(power.compareTo(BigInteger.ZERO) < 0) {
+            const one = new FieldElementBN(BigInteger.ONE, this.prime)
+            const divisor = new FieldElementBN(this.num, this.prime).pow(power.negate())
+            return one.div(divisor)
+        }
         return new FieldElementBN(this.num.modPow(new BigInteger(power.toString()), this.prime), this.prime)
+    }
+
+    public div(fieldElement: FieldElementBN): FieldElementBN {
+        if(!fieldElement.prime.equals(this.prime)) {
+            throw new Error('Elements are not on the same prime field')
+        }
+        const primeLessTwo = this.prime.subtract(this.TWO).mod(this.prime)
+        const quotient = fieldElement.num.modPow(primeLessTwo, this.prime)
+        const multiplier = this.num
+        const result = multiplier.multiply(quotient).mod(this.prime)
+        return new FieldElementBN(result, this.prime)
+
     }
 }
--- src/lib/tests/FieldElementBN.unit.ts
+++ src/lib/tests/FieldElementBN.unit.ts
@@ -54,9 +54,19 @@ describe('FieldElementBN', () => {
         const fe = newFieldElement(5,7)
 
         describe('success', () => {
-            it('should add two elements', () => {
-                const fe2 = newFieldElement(3, 7)
-                expect(fe.add(fe2)).toEqual(newFieldElement(1,7))
+            it('should add two elements whose sum is less than prime field order', () => {
+                const a = newFieldElement(2,31)
+                const b = newFieldElement(15,31)
+                expect(a.add(b)).toEqual(newFieldElement(17,31))
+                //
+                // const fe2 = newFieldElement(3, 7)
+                // expect(fe.add(fe2)).toEqual(newFieldElement(1,7))
+            })
+
+            it('should add two elements whose sum is greater than prime field order', () => {
+                const a = newFieldElement(17,31)
+                const b = newFieldElement(21,31)
+                expect(a.add(b)).toEqual(newFieldElement(7,31))
             })
         })
         describe('failure', () => {
@@ -68,35 +78,43 @@ describe('FieldElementBN', () => {
     })
 
     describe('subtraction', () => {
-        const fe = newFieldElement(5, 7)
         describe('success', () => {
-            it('should return a new element that is the difference between the two', () => {
-                const fe2 = newFieldElement(2, 7)
-                expect(fe.sub(fe2)).toEqual(newFieldElement(3, 7))
+            it('should subtract an element from another where the difference is less than the prime field order', () => {
+                const a = newFieldElement(29,31)
+                const b = newFieldElement(4,31)
+                expect(a.sub(b)).toEqual(newFieldElement(25,31))
+            })
+            it('should subtract an element from another where the difference is greater than the prime field order', () => {
+                const a = newFieldElement(15,31)
+                const b = newFieldElement(30,31)
+                expect(a.sub(b)).toEqual(newFieldElement(16,31))
             })
         })
 
         describe('error', () => {
             it('should throw an error if elements are not of the same prime field', () => {
+                const fe1 = newFieldElement(29, 31)
                 const fe2 = newFieldElement(2, 11)
-                expect(() => fe.sub(fe2)).toThrow(Error)
+                expect(() => fe1.sub(fe2)).toThrow(Error)
             })
         })
     })
 
     describe('multiplication', () => {
-        const fe = newFieldElement(5, 7)
+
         describe('success', () => {
             it('should return a new element that is the product of the two', () => {
-                const fe2 = newFieldElement(2, 7)
-                expect(fe.mul(fe2)).toEqual(newFieldElement(3, 7))
+                const a = newFieldElement(24, 31)
+                const b = newFieldElement(19, 31)
+                expect(a.mul(b)).toEqual(newFieldElement(22, 31))
             })
         })
 
         describe('error', () => {
             it('should throw an error if elements are not of the same prime field', () => {
+                const fe1 = newFieldElement(2, 31)
                 const fe2 = newFieldElement(2, 11)
-                expect(() => fe.mul(fe2)).toThrow(Error)
+                expect(() => fe1.mul(fe2)).toThrow(Error)
             })
         })
     })
@@ -105,11 +123,42 @@ describe('FieldElementBN', () => {
         const fe = newFieldElement(17, 31)
         const fe2 = newFieldElement(18,31)
         it('should return a new element that is exponentially larger', () => {
-            expect(fe.pow(3)).toEqual(newFieldElement(15,31))
+            expect(fe.pow(new BigInteger('3'))).toEqual(newFieldElement(15,31))
+        })
+
+        it('should perform negative exponentiation', () => {
+            const a = newFieldElement(17,31)
+            expect(a.pow(new BigInteger('-3'))).toEqual(newFieldElement(29, 31))
         })
 
         it('should chain properly with multiplication', () => {
-            expect(newFieldElement(5,31).pow(5).mul(fe2)).toEqual(newFieldElement(16,31))
+            expect(newFieldElement(5,31).pow(new BigInteger('5')).mul(fe2)).toEqual(newFieldElement(16,31))
         })
+    })
+
+    describe('division', () => {
+
+        describe('success', () => {
+            it('should divide a by b', () => {
+                const a = newFieldElement(3,31)
+                const b = newFieldElement(24,31)
+                expect(a.div(b)).toEqual(newFieldElement(4, 31))
+            })
+
+            it('should work with complex operations', () => {
+                const a = newFieldElement(4, 31)
+                const b = newFieldElement(11, 31)
+                expect(a.pow(new BigInteger('-4')).mul(b)).toEqual(newFieldElement(13,31))
+            })
+        })
+
+        describe('error', () => {
+            it('should throw an error if elements are not of the same prime field', () => {
+                const fe1 = newFieldElement(2, 31)
+                const fe2 = newFieldElement(2, 11)
+                expect(() => fe1.div(fe2)).toThrow(Error)
+            })
+        })
+
     })
 })
